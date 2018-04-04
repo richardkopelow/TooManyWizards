48d6c99234c2d8e68b4f94e33e1b451e00b6e9f2
Richard Kopelow <richardkopelow@gmail.com>
2018-04-04 11:04:06 -0400

    Fixed a die side checking bug Dice get destroyed when they stop

--- Assets/_Scripts/Gameplay/Die.cs
+++ Assets/_Scripts/Gameplay/Die.cs
@@ -7,13 +7,17 @@ public class Die : MonoBehaviour
     public float MaxStrength;
     public float MinStrength;
     public Vector3 LaunchDirection;
+    public GameObject VCam;
 
-    Rigidbody rigid;
+    private Transform trans;
+    private Rigidbody rigid;
     private int castMask;
     private Vector3[] directions;
+    private bool destroy;
 
     private void Start()
     {
+        trans = GetComponent<Transform>();
         rigid = GetComponent<Rigidbody>();
         castMask = LayerMask.GetMask("Ground");
         directions = new Vector3[] {
@@ -28,8 +32,9 @@ public class Die : MonoBehaviour
 
     public Coroutine Roll(CoroutineOut<int> result)
     {
+        VCam.SetActive(true);
         Coroutine rollCo = StartCoroutine(roll(result));
-        //StartCoroutine(destroyAfter(rollCo));
+        StartCoroutine(destroyCoroutine());
         return rollCo;
     }
 
@@ -37,7 +42,7 @@ public class Die : MonoBehaviour
     {
         yield return null;// new WaitForFixedUpdate();
         rigid.rotation = Random.rotationUniform;
-        rigid.velocity = (LaunchDirection.normalized + Vector3.right * Random.Range(-1, 1)) * Random.Range(MinStrength, MaxStrength);
+        rigid.velocity = (LaunchDirection.normalized + Vector3.right * Random.Range(-0.3f, 0.3f)) * Random.Range(MinStrength, MaxStrength);
         rigid.angularVelocity = Random.onUnitSphere * Random.Range(5, 10);
         yield return null;// new WaitForFixedUpdate();
         yield return null;// new WaitForFixedUpdate();
@@ -45,18 +50,28 @@ public class Die : MonoBehaviour
         {
             yield return null;//new WaitForFixedUpdate();
         }
+        yield return new WaitForSeconds(3);
+        float minDist = float.MaxValue;
         for (int i = 0; i < directions.Length; i++)
         {
-            if (Physics.Raycast(rigid.position, directions[i], castMask))
+            RaycastHit hit;
+            if (Physics.Raycast(rigid.position, trans.TransformDirection(directions[i]), out hit, 3, castMask))
             {
-                result.Data = i;
+                if (hit.distance < minDist)
+                {
+                    minDist = hit.distance;
+                    result.Data = i + 1;
+                }
             }
         }
         result.Done = true;
+        VCam.SetActive(false);
+        destroy = true;
     }
 
-    private IEnumerator destroyAfter(Coroutine co)
+    private IEnumerator destroyCoroutine()
     {
-        yield return co;
+        yield return new WaitUntil(()=>destroy);
+        Destroy(gameObject);
     }
 }
